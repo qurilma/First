d982525940ec7b037f3b617140e9e6439115028d
Marat <qurilma2012@yandex.com>
2019-03-04 17:57:17 +0500

    Урок 008 условия

---
+++ script.js
@@ -0,0 +1,29 @@
+let num = 50;
+
+if (num < 49) {
+    console.log('Неверно!')
+} else if (num > 100) {
+    console.log("Много!")
+} else {
+    console.log('верно!')
+};
+
+(num == 50 ) ? console.log('Верно!') : console.log("Неверно!");
+
+switch (num) {
+    case num < 49:
+        console.log('Неверно!');
+        break;
+    case num > 100:
+        console.log('много!');
+        break;
+    case num > 80:
+        console.log('Все еще много!');
+        break;
+    case 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.loge("Что-то пошло не так!");
+        break;
+}
